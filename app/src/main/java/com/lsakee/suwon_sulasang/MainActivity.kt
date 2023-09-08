6ab4d90972dc23580795a5a8f72806f69223d401
lsakee <93514333+lsakee@users.noreply.github.com>
2023-09-08 17:16:17 +0900

    init src

---
+++ app/src/main/java/com/lsakee/suwon_sulasang/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lsakee.suwon_sulasang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
